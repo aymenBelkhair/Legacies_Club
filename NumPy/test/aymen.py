ddc72060c8ff340557043aef3498b642c4db5286
aymenBelkhair <contact.aymenbelkhair@gmail.com>
2024-05-01 19:47:52 +0100

    A new presentation for legacies member

---
+++ NumPy/test/aymen.py
@@ -0,0 +1,12 @@
+######################################
+### 
+### the CLASS module
+###
+######################################
+
+
+teacherName = "Franck Iutzeler"
+teacherWebsite = "http://www.iutzeler.org"
+
+def somation(a,b):
+	return a+b
